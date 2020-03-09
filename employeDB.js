87028d84492d8028dc7ef5d3e14adacafada9f64
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-03-08 17:33:45 -0700

    created the add, view and update functions

--- employeDB.js
+++ employeDB.js
@@ -23,3 +23,31 @@ connection.connect(function(err) {
   //run the start function after the connection is made to prompt the user
   start();
 });
+
+// function which prompst the user for what action they should take
+function start() {
+  inquirer
+  .prompt({
+    name: "addOrViewOrUpdate",
+    type: "list",
+    message: "Would you like to [ADD] an employee [VIEW] an employee [UPDATE] an employee?",
+    choices: ["ADD", "VIEW, UPDATE"]
+  })
+  .then(function(annser) {
+    // based on their answer, either call the add or the view or the update functions
+    if (answer.addOrViewOrUpdate === "ADD") {
+      addEmployee();
+    }
+    else if(answer.addOrViewOrUpdate === "VIEW") {
+      viewEmployee();
+    }
+    else if (answer.addOrViewOrUpdate === "UPDATE") {
+      updateEmployee();
+    }
+    else{
+      connection.end();
+    }
+  });
+}
+console.log()
+// function to handle adding new employee
